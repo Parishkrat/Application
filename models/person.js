2fd57ae35021afa07f3f9d3536c8b685f8891354
Parishkrat <parish.mishra2001@gmail.com>
2025-09-17 23:30:21 +0530

    Update person.js

--- models/person.js
+++ models/person.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  email: String,
+  name: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  inviteToken: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
 });
 
 module.exports = mongoose.model("Person", personSchema);
