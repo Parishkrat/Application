f9386d6617ff6be8fb1c0bff4e32e5dc865e4e87
Parishkrat <parish.mishra2001@gmail.com>
2025-09-17 23:30:40 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,8 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  name: { type: String },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: { type: String, required: true },
+  isPaid: { type: Boolean, default: false },
+  plan: { type: String, enum: ["free", "pro"], default: "free" },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
